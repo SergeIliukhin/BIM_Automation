6a20782fdaad8c91790a1511e3489a00e47d224c
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-11-19 20:09:06 +0300

    Решение задачи 4.1

---
+++ Task04_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное число:");           
+            int number = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 1; i <= (2*number - 1); i+=2)
+            {
+                result += i;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
