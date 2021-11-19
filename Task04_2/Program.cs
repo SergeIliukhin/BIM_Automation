f4323d5674f307569d4e17364cb7085ca96d1fc2
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-11-19 20:45:22 +0300

    Решение задачи 4.2

---
+++ Task04_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int positiveNumbers = 0;
+            int negativeNumbers = 0;
+            int stop = 0;
+            Console.WriteLine("Введите любое целое число:");
+            do
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > 0)
+                {
+                    positiveNumbers += 1;                    
+                }
+                else if (number < 0)
+                {
+                    negativeNumbers += 1;                    
+                }
+                else
+                    stop = 1;                    
+            } while (stop != 1);
+            if (positiveNumbers > negativeNumbers)
+            {
+                Console.WriteLine($"Положительных чисел ({positiveNumbers}) больше, чем отрицательных ({negativeNumbers}).");
+            }
+            else if (positiveNumbers < negativeNumbers)
+            {
+                Console.WriteLine($"Положительных чисел ({positiveNumbers}) меньше, чем отрицательных ({negativeNumbers}).");
+            }
+            else
+            {
+                Console.WriteLine($"Положительных чисел столько же, сколько отрицательных ({positiveNumbers}).");
+            }
+            Console.ReadKey();
+        }
+    }
+}
