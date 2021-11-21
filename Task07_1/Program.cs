6654557e38c92b7b64aaebc7bdf3772c3d1770ae
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-11-22 01:01:34 +0300

    Задачи к уроку 7

---
+++ Task07_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task07_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательно длины сторон первого треугольника:");
+            double firstA = double.Parse(Console.ReadLine());
+            double firstB = double.Parse(Console.ReadLine());
+            double firstC = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите последовательно длины сторон первого треугольника:");
+            double secondA = double.Parse(Console.ReadLine());
+            double secondB = double.Parse(Console.ReadLine());
+            double secondC = double.Parse(Console.ReadLine());
+
+            double firstSquare = GetSquare(firstA, firstB, firstC);
+            double secondSquare = GetSquare(secondA, secondB, secondC);
+
+            if (firstSquare > secondSquare)
+                Console.WriteLine($"Площадь первого треугольника ({firstSquare}) больше площади второго треугольника ({secondSquare}).");
+            else if (secondSquare > firstSquare)
+                Console.WriteLine($"Площадь первого треугольника ({firstSquare}) меньше площади второго треугольника ({secondSquare}).");
+            else
+                Console.WriteLine($"Площади треугольников равны и составляют: {firstSquare}.");
+
+            Console.ReadKey();
+        }
+
+
+        static double GetSquare (double a, double b, double c)
+        {
+            double halfPerimeterOfTriangle = 0.5 * (a + b + c);
+            double squareOfTriangle = Math.Round(Math.Sqrt(halfPerimeterOfTriangle * (halfPerimeterOfTriangle - a) * (halfPerimeterOfTriangle - b) * (halfPerimeterOfTriangle - c)), 3, MidpointRounding.AwayFromZero);
+            return squareOfTriangle;
+        }
+
+    }
+}
---
+++ Task07_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task07_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны куба:");
+            double side = double.Parse(Console.ReadLine());
+
+            GetCubeParameters(side, out double volume, out double sidesSquare);
+
+            Console.WriteLine($"Объем куба составляет: {volume}.");
+            Console.WriteLine($"Площадь поверхности куба составляет: {sidesSquare}.");
+
+            Console.ReadKey();
+        }
+        static void GetCubeParameters(double side, out double volume, out double sidesSquare)
+        {
+            volume = Math.Round(Math.Pow(side, 3), 3, MidpointRounding.AwayFromZero);
+            sidesSquare = Math.Round(6 * Math.Pow(side, 2), 3, MidpointRounding.AwayFromZero);
+        }
+    }
+}
