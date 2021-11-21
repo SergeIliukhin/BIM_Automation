c9434173b278a4473a9169ef9d93ff06e12dc12e
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-11-21 20:50:30 +0300

    Задачи к уроку 6

---
+++ Task06_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string str = Console.ReadLine();
+            string[] strArr = str.Split();
+            int wordLen = 0;
+            string strResult = "";
+            foreach (string item in strArr)
+            {
+                if (item.Length > wordLen)
+                {
+                    wordLen = item.Length;
+                    strResult = item;
+                }
+            }
+            Console.WriteLine($"Самое длинное слово в предложении: \"{strResult}\". Количество символов: {wordLen}.");
+        }
+    }
+}
