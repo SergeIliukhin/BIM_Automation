c1becb6b520b822e25da4e0efc79dd34ad5c0cf2
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-11-26 20:21:57 +0300

    Добавлено условие для ситуации, когда в строке несколько слов с одинаковой длиной

--- Task06_1/Program.cs
+++ Task06_1/Program.cs
@@ -10,20 +10,45 @@ namespace Task06_1
     {
         static void Main(string[] args)
         {
+            //Защиту от пустого предложения не предусматриваем
+
             Console.WriteLine("Введите предложение:");
             string str = Console.ReadLine();
             string[] strArr = str.Split();
-            int wordLen = 0;
+            
+            int wordLongestLen = 0;
+            int countLongestWords = 0;
             string strResult = "";
+            //string strResultMultiple = "";
+
+            //Считаем максимальное число символов отдельно взятого слова в строке
             foreach (string item in strArr)
             {
-                if (item.Length > wordLen)
+                if (item.Length > wordLongestLen)
                 {
-                    wordLen = item.Length;
-                    strResult = item;
+                    wordLongestLen = item.Length;                    
+                }                
+            }
+
+            //Считаем количество слов с максимальной длиной и добавляем их в итоговую строку с ответом
+            foreach (string item in strArr)
+            {
+                if (item.Length == wordLongestLen)
+                {
+                    countLongestWords += 1;
+                    strResult = strResult + item + ", ";
                 }
             }
-            Console.WriteLine($"Самое длинное слово в предложении: \"{strResult}\". Количество символов: {wordLen}.");
+
+            string strResultTrimmed = strResult.Remove(strResult.Length - 2, 2);
+
+            //Выводим результат в зависимости от того, сколько слов с одинаковой длиной нашлось
+            if (countLongestWords == 1)
+                Console.WriteLine($"Самое длинное слово в предложении: \"{strResultTrimmed}\". Количество символов: {wordLongestLen}.");
+            else
+                Console.WriteLine($"Самые длинные слова в предложении: \"{strResultTrimmed}\". Количество символов: {wordLongestLen}.");
+
+            Console.ReadKey();
         }
     }
 }
