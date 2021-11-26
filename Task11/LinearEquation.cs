a8950ac403df4715b124522c7d5ee19d5477c9f7
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-11-26 13:04:47 +0300

    Решение задачи 11

---
+++ Task11/LinearEquation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    public struct LinearEquation
+    {
+        public double k;
+        public double b;
+        private double x;
+        
+        public void Root()
+        {            
+            if (k != 0)
+            {
+                x = Math.Round(-k / b, 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"X равен {x}.");
+            }
+            else if (k == 0 && b != 0)
+            {
+                Console.WriteLine("Решений уравнения нет.");
+            }
+            else if (k == 0 && b == 0)
+            {
+                Console.WriteLine("Решением уравнения может быть любое число.");
+            }            
+        }
+    }
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дано линейное уравнение kx + b = 0. Требуется найти \"x\" при заданных параметрах \"k\" и \"b\".");
+            Console.WriteLine("Введите значение \"k\":");
+            double k = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение \"b\":");
+            double b = double.Parse(Console.ReadLine());
+
+            LinearEquation lin = new LinearEquation();
+            lin.k = k;
+            lin.b = b;
+
+            lin.Root();
+
+            Console.ReadKey();
+
+        }
+    }
+}
