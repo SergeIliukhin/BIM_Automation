ca19f57d934352368a6d6edbb5e70a7ca748263c
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-12-01 14:54:36 +0300

    Решение задачи 12

---
+++ Task12/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    public static class Circle
+    {
+        static double circleRadius;
+        static double x;
+        static double y;
+
+        public static void CircleLength(double radius)
+        {
+            circleRadius = radius;
+            double circleLength = Math.Round((2 * Math.PI * circleRadius), 2, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"При заданном радиусе длина окружности составляет {circleLength}");
+        }
+        public static void CircleSquare(double radius)
+        {
+            circleRadius = radius;
+            double circleSquare = Math.Round((Math.PI * Math.Pow(circleRadius, 2)), 2, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"При заданном радиусе площадь окружности составляет {circleSquare}");
+        }
+        public static void PointBelong(double userX, double userY)
+        {
+            x = userX;
+            y = userY;
+            if (x <= circleRadius && y <= circleRadius)
+                Console.WriteLine($"Заданная вами точка находится в пределах окружности с радиусом {circleRadius} и цетром в начале координат.");
+            else
+                Console.WriteLine($"Заданная вами точка находится вне пределов окружности с радиусом {circleRadius} и цетром в начале координат.");
+        }
+
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значени радиуса окружности:");
+            double radius = double.Parse(Console.ReadLine());
+
+            Circle.CircleLength(radius);
+            Circle.CircleSquare(radius);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите последовательно две координаты точки \"x\" и \"y\":");
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            Circle.PointBelong(x, y);
+
+            Console.ReadKey();
+        }
+    }
+}
