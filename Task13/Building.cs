69e7ea83a1c2be6718cb03c6d70cf1ad0bed1c18
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-12-02 03:18:54 +0300

    Решение задачи 13

---
+++ Task13/Building.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13
+{
+    
+    public class Building
+    {
+        protected string Adress { get; private set; }
+        protected double Length { get; private set; }
+        protected double Width { get; private set; }
+        protected double Height { get; private set; }
+
+        public Building(string adress, double length, double width, double height)
+        {
+            Adress = adress;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Здание расположено по адресу: {Adress}.\nДлина здания: {Length}.\nШирина здания: {Width}.\nВысота здания: {Height}.");
+        }
+    }
+    
+
+    
+    public sealed class MultiBuilding : Building
+    {
+        private double Levels { get; set; }
+        public MultiBuilding(string adress, double length, double width, double height, double levels)
+            : base(adress, length, width, height)
+        {
+            Levels = levels;
+        }
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Количество этажей в здании: {Levels}.");
+        }
+    }
+    
+    
+}
---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building bld = new Building("Адрес", 100, 50, 25);
+
+            bld.Print();
+
+            Console.WriteLine();
+
+            MultiBuilding mBld = new MultiBuilding("Адрес", 100, 50, 25, 8);
+
+            mBld.Print();
+
+            Console.ReadKey();
+        }
+    }
+
+}
