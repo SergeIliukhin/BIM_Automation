15e9d06117990e352e91092b5f280da72f7c148e
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-12-07 03:28:44 +0300

    Решение задачи 14

---
+++ Task14/Animal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14
+{
+    public abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+        
+        public void ShowInfo()
+        {
+            Console.Write($"Название животного: {Name}, его голос: ");
+            Say();
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override string Name { get; set; }       
+
+        public Cat(string name)
+            : base(name)
+        {
+            
+        }
+
+        public override void Say()
+        {
+            Console.Write("Мяу.");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override string Name { get; set; }
+
+        public Dog(string name)
+            : base(name)
+        {
+
+        }
+
+        public override void Say()
+        {
+            Console.Write("Гав.");
+        }
+    }
+
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Кошка");           
+            cat.ShowInfo();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Animal dog = new Dog("Собака");
+            dog.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
