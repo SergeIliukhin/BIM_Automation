2dea6242a0101841974f2bb863f415f819b5d8d3
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-12-10 18:55:30 +0300

    Решение задачи 15

---
+++ Task 15/GeomProgression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_15
+{
+    public class GeomProgression : ISeries
+    {
+        public int Start { get; private set; }
+        public int Item { get; private set; }
+
+        public void SetStart(int x)
+        {
+            Start = x;
+            Item = Start;
+        }
+        public int GetNext()
+        {
+            //можно запросить шаг
+            Item *= 2;
+            return Item;
+        }
+
+        public void Reset()
+        {
+            Item = Start;
+        }
+
+
+    }
+}
---
+++ Task 15/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задаем случайное начало ряда
+            Random rnd = new Random();
+            int rndStart = rnd.Next(0, 100);
+
+            ArithProgression arithProgression = new ArithProgression();
+            arithProgression.SetStart(rndStart);
+
+            GeomProgression geomProgression = new GeomProgression();
+            geomProgression.SetStart(rndStart);
+
+
+            Console.WriteLine($"Первый ряд арифметической прогрессии с начальным значением {rndStart} и шагом 5:");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write(arithProgression.GetNext() + " ");
+            }
+
+            Console.WriteLine(
+                );
+            Console.WriteLine($"Первый ряд геометической прогрессии с начальным значением {rndStart} и коэффициентом 2:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(geomProgression.GetNext() + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            arithProgression.Reset();
+
+            rndStart = rnd.Next(0, 100);
+
+            arithProgression.SetStart(rndStart);
+
+            Console.WriteLine($"Второй ряд арифметической прогрессии с начальным значением {rndStart} и шагом 5:");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write(arithProgression.GetNext() + " ");
+            }
+
+            Console.WriteLine();
+            
+            Console.WriteLine($"Второй ряд геометической прогрессии с начальным значением {rndStart} и коэффициентом 2:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(geomProgression.GetNext() + " ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
