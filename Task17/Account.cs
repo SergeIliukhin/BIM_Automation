7b1db1e1cdbd6ae4fd7933b4724b4d7a0b364c72
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-12-14 19:05:37 +0300

    Решение задачи 17

---
+++ Task17/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    public class Account<T>
+    {
+        public T ID { get; private set; }
+        public decimal Sum { get; private set; }
+        public string Name { get; private set; }
+
+        public Account(T id, decimal sum, string name)
+        {
+            ID = id;
+            Sum = sum;
+            Name = name;
+        }
+
+       public void PrintDetails<T>(T id, decimal sum, string name)
+        {
+            
+            Console.WriteLine($"Данные счета пользователя {name} -  ID: {id}, сумма на счету: {sum}.");
+        }
+
+        public void ChangeSum(decimal sum, string name)
+        {
+            Sum = sum;
+
+            Console.WriteLine($"Новая сумма на счету пользователя {name}: {sum}.");
+        }
+    }
+}
---
+++ Task17/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //защиту от неправильного ввода суммы на счете не предусматриваем
+
+            Console.WriteLine("Введите данные счета в формате Номер/Сумма/ФИО.");
+
+            string data = Console.ReadLine();
+
+            string[] dataSplitted = data.Split('/');
+            
+           
+            int numberID;           
+           
+            bool parsedID = int.TryParse(dataSplitted[0], out numberID);
+
+            if (parsedID == true)
+            {
+                Account<int> person = new Account<int>(numberID, decimal.Parse(dataSplitted[1]), dataSplitted[2]);
+                person.PrintDetails(person.ID, person.Sum, person.Name);
+                Console.WriteLine("Введите новую сумму на счете:");
+                decimal newSum = decimal.Parse(Console.ReadLine());
+                person.ChangeSum(newSum, person.Name);
+            }
+            else
+            {
+                Account<string> person = new Account<string>(dataSplitted[0], decimal.Parse(dataSplitted[1]), dataSplitted[2]);
+                person.PrintDetails(person.ID, person.Sum, person.Name);
+                Console.WriteLine("Введите новую сумму на счете:");
+                decimal newSum = decimal.Parse(Console.ReadLine());
+                person.ChangeSum(newSum, person.Name);
+            }
+
+            Console.ReadKey();            
+
+        }
+    }
+}
