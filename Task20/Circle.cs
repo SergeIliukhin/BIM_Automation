5e5276b77d685c418adfc0142270b5cf6925efc1
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-12-21 11:49:10 +0300

    Решение задачи 20

---
+++ Task20/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task20
+{
+    class Circle
+    {
+        public static double Radius { get; private set; }
+        public static double GetCircleLength (double radius)
+        {
+            Radius = radius;
+            return Math.Round(2 * Math.PI * Radius, 2, MidpointRounding.AwayFromZero);
+        }
+        public static double GetCircleSquare(double radius)
+        {
+            Radius = radius;
+            return Math.Round(Math.PI * Math.Pow(Radius,2), 2, MidpointRounding.AwayFromZero);
+        }
+        public static double GetSphereVolume(double radius)
+        {
+            Radius = radius;
+            return Math.Round((4 * Math.PI * Math.Pow(Radius, 3) / 3), 2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ Task20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task20
+{
+    class Program
+    {
+        delegate double Calculation(double radius);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус:");
+            double radius = double.Parse(Console.ReadLine());
+
+            Calculation calcCircleLength = Circle.GetCircleLength;
+            Calculation calcCircSquare = Circle.GetCircleSquare;
+            Calculation calcSphereVolume = Circle.GetSphereVolume;
+
+            Console.WriteLine("Длина окружности равна: " + calcCircleLength(radius) + ".");
+            Console.WriteLine("Площадь круга равна: " + calcCircSquare(radius) + ".");
+            Console.WriteLine("Объем шара равен: " + calcSphereVolume(radius) + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
