efdfa8cb4b8eddf35a9f2041951a47dbb6373089
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2021-12-27 20:17:52 +0300

    Решение задачи 22

---
+++ Task22/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите количество элементов массива:");
+            int numberElements = int.Parse(Console.ReadLine());
+            
+            int[] array = new int[numberElements];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+            }
+
+            Console.WriteLine("Исходный массив:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            int sum = 0;
+                        
+            Action action = new Action(GetSum);
+            Task task1 = new Task(action);
+
+            Action<Task> actionTask = new Action<Task>(GetMaxValue);
+            Task task2 = task1.ContinueWith(actionTask);
+
+            task1.Start();
+
+            void GetSum()
+            {                
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];                    
+                }
+                Console.WriteLine($"Сумма всех элементов в массиве: {sum}.");                
+            }
+
+            void GetMaxValue(Task task)
+            {
+                Console.WriteLine($"Максимальное значение в массиве: {array.Max()}.");
+            }
+
+             
+            Console.ReadKey();
+        }
+    }
+}
