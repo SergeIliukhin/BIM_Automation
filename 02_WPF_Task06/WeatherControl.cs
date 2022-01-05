683c32601e43ef0d1bf24ca5ec6f64c49b1161c3
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2022-01-06 00:27:34 +0300

    Решение задачи 6

---
+++ 02_WPF_Task06/WeatherControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _02_WPF_Task06
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty;
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+        public string WindDirection { get; set; }
+        public int WindVelocity { get; set; }
+
+        enum RainfallExistence : byte
+        {
+            Солнечно = 0,
+            Облачно = 1,
+            Дождь = 2,
+            Снег = 3
+        }
+
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.Journal,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)),
+                new ValidateValueCallback(ValidateTemperature));
+        }
+
+        private static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+                return true;
+            else
+                return false;
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+                return v;
+            else
+                return 0;
+        }
+    }
+}
