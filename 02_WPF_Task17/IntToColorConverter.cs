b832f05c30ec056dab592738d1abeefcaf15f316
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2022-01-20 00:55:39 +0300

    Решение задачи 17

---
+++ 02_WPF_Task17/IntToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace _02_WPF_Task17
+{
+    public class IntToColorConverter : IMultiValueConverter
+    {
+        #region IMultiValueConverter Members
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(Color.FromRgb(
+                (byte)(double)values[0],
+                (byte)(double)values[1],
+                (byte)(double)values[2]));
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ 02_WPF_Task17/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace _02_WPF_Task17
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sliderRed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            labelRed.Content = "Red (" + (Math.Round(sliderRed.Value,0)).ToString() + "):";
+        }
+
+
+        private void sliderGreen_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            labelGreen.Content = "Green (" + (Math.Round(sliderGreen.Value, 0)).ToString() + "):";
+        }
+
+        private void sliderBlue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            labelBlue.Content = "Blue (" + (Math.Round(sliderBlue.Value, 0)).ToString() + "):";
+        }
+    }
+
+}
