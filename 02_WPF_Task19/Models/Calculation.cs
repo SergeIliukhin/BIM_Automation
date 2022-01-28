45fa1743d03505c5444eb564b8080708a1435b4c
Сергей Илюхин <s.e.iliukhin@yandex.ru>
2022-01-28 12:06:09 +0300

    Решение задачи 19

---
+++ 02_WPF_Task19/Models/Calculation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_WPF_Task19.Models
+{
+    public static class Calculation
+    {
+        public static double Calculate(double r)
+        {
+            return Math.Round(2 * Math.PI * r, 2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ 02_WPF_Task19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using _02_WPF_Task19.Annotations;
+using _02_WPF_Task19.Models;
+
+namespace _02_WPF_Task19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double result;
+        public double Result
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            Result = Calculation.Calculate(Radius);
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (radius != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+
+    }
+}
